582a1c64f400d1e688ca39e98b2c5326325429d4
Gayle Hoefer <gaylehoefer@gmail.com>
2024-07-27 21:26:24 -0700

    added class

---
+++ Classes and pointers/classes.java
@@ -0,0 +1,19 @@
+//class
+public class Cookie {
+    private String color;
+
+    //Constructor
+    public Cookie(String color) {
+        this.color = colorl
+    }
+
+    public String getColor() {
+        return color;
+    }
+    public void serColor(String color) {
+        this.color = color;
+    }
+} 
+
+Cookie cookieOne = new Cookie("green");
+Cookie cookieTwo = new Cookie("blue");
