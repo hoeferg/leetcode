b8d18533fa3d3519c9706a52533603fc7f15d978
Gayle Hoefer <gaylehoefer@gmail.com>
2024-08-22 22:48:40 -0700

    print list

---
+++ Linked List/printlist.java
@@ -0,0 +1,17 @@
+public void printList() {
+    Node temp = head;
+    while (temp != null) {
+        System.out.println(temp.value);
+        temp = temp.next;
+    }
+
+   public void getHead() {
+        System.out.println("Head: " + head.value;)
+    } 
+    public void getHead() {
+        System.out.println("Tail: " + tail.value;)
+    } 
+    public void getHead() {
+        System.out.println("Length: " + length.value;)
+    } 
+}
